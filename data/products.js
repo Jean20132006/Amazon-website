7055a4b7405422ec3702efe2194d42c972a9fc61
Jean20132006 <jeanerictsanga8@gmail.com>
2026-01-10 01:01:38 -0500

    first row

---
+++ data/products.js
@@ -0,0 +1,86 @@
+products = [
+    { name: "CORE_HP_Trending_Pre-loved_handbags_5",
+      image: "images/products-img/CORE_HP_Trending_Pre-loved_handbags_5.jpg",
+      price: 19.99,
+      id: "186x116._SY116_CB804224994",
+      description: "Chanel",
+      category: "bags"
+    },{
+        name: "CORE_HP_Pre-loved_jewelry",
+        image: "images/products-img/CORE_HP_Pre-loved_jewelry_6.jpg",
+        price: 29.99,
+        id: "186x116._SY116_CB803997880",
+        description: "Hermes",
+        category: "Jewelry"
+    },{
+        name: "CORE_HP_Pre-loved_jewelry",
+        image: "images/products-img/CORE_HP_Pre-loved_jewelry_7.jpg",
+        price: 15.99,
+        id: "186x116._SY116_AB803997880",
+        description: "Van Cleef",
+        category: "Jewelry"
+    },{
+        name: "CORE_HP_Trending_Pre-loved_handbags_8",
+        image: "images/products-img/CORE_HP_Trending_Pre-loved_handbags_8.jpg",   
+        price: 12.99,
+        id: "186x116._SY116_CB804224994",
+        description: "A gentle facial cleanser that removes impurities without stripping moisture.",
+        category: "Bags"
+    },{
+        name: "Desktop_QC_TopLeft_Editorial_5",
+        image: "images/products-img/Desktop_QC_TopLeft_Editorial_5.jpg",
+        price: 22.99,
+        id: "1x._SY116_CB774835635",
+        description: "Dolce&Gabbana",
+        category: "dress"
+    },{
+        name: "Desktop_QC_BottomRight_Editorial_5",
+        image: "images/products-img/Desktop_QC_BottomRight_Editorial_5.jpg",
+        price: 18.99,
+        id: "1x5._SY116_CB774835635",
+        description: "Balmain",
+        category: "T-shirt"
+    },{
+        name: "Desktop_QC_BottomLeft_Editorial_4",
+        image: "images/products-img/Desktop_QC_BottomLeft_Editorial_4.jpg",   
+        price: 14.99,
+        id: "1x12._SY116_CB775366018",
+        description: "Stella McCartney",    
+        category: "bags"
+    },{
+        name: "Desktop_QC_TopRight_Editorial_5",
+        image: "images/products-img/Desktop_QC_TopRight_Editorial_5.jpg",   
+        price: 16.99,
+        id: "1x.25_SY116_CB774835635",
+        description: "Pierre Hardy",
+        category: "shoes"
+    },{
+        name: "PC",
+        image: "images/products-img/PC.jpg",
+        price: 19.99,
+        id: "18x116._SY116_CB561026793",
+        description: "Computer",
+        category: "computers"
+    },{
+        name: "XCM_CUTTLE",
+        image: "images/products-img/XCM_CUTTLE.jpg",
+        price: 24.99,
+        id: "186x00_1X_en_US._SY116_CB561559219",
+        description: "Software",
+        category: "software"
+    },{
+        name: "Wireless",
+        image: "images/products-img/Wireless.jpg",
+        price: 29.99,
+        id: "223x116._SY116_TB561026793_",
+        description: "Headphones",
+        category: "accessories"
+    },{
+        name: "HomeEntertainment",
+        image: "images/products-img/HomeEntertainment.jpg",
+        price: 39.99,
+        id: "186x116._SY116_kA561026793",
+        description: "TVs",
+        category: "electronics"
+    }
+];
